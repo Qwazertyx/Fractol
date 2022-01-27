0f31b77bb300719c40300d917cd9e0235212f3d2
Victor Sedat <50156809+Qwazertyx@users.noreply.github.com>
2022-01-27 01:34:09 +0100

    Update fractol.c

--- srcs/fractol.c
+++ srcs/fractol.c
@@ -6,7 +6,7 @@
 /*   By: Cathy <Cathy@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 10:06:39 by vsedat            #+#    #+#             */
-/*   Updated: 2022/01/25 17:58:23 by Cathy            ###   ########lyon.fr   */
+/*   Updated: 2022/01/26 03:45:16 by Cathy            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,22 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 void	mandelbrot(t_data *data, int px, int py)
 {
-	float	x;
-	float	y;
-	float	temp;
+	double	x;
+	double	y;
+	double	temp;
 	int		i;
 
 	x = 0;
 	y = 0;
 	i = 0;
-	while (x * x + y * y <= 2 * 2 && i < 1000)
+	while (x * x + y * y <= 2 * 2 && i < 100)
 	{
-		temp = (x * x - y * y + (px * (0.005 * data->zoom) - (2 * data->updown)));
-		y = (2 * x * y + (py * (0.005 * data->zoom) - (1.5 * data->leftright)));
+		temp = (x * x - y * y + (px * (0.005 * data->zoom1) - (2 * data->updown * data->zoom2)));
+		y = (2 * x * y + (py * (0.005 * data->zoom3) - (1.5 * data->leftright * data->zoom4)));
 		x = temp;
 		i++;
 	}
-	my_mlx_pixel_put(data, px, py, 5000 - i * 5);
+	my_mlx_pixel_put(data, px, py, 9000 - i * 5);
 }
 
 int	algo(t_data *data)
@@ -63,6 +63,20 @@ int	algo(t_data *data)
 	return (0);
 }
 
+void	vardef(t_data *data)
+{
+	data->winwidth = 600;
+	data->winlength = 600;
+	data->updown = 1;
+	data->leftright = 1;
+	data->zoom1 = 1;
+	data->zoom2 = 1;
+	data->zoom3 = 1;
+	data->zoom4 = 1;
+	data->mousex = 1;
+	data->mousey = 1;
+}
+
 int	close(t_data *data)
 {
 	mlx_destroy_window(data->mlx, data->win);
@@ -82,22 +96,38 @@ int	key_hook(int keycode, t_data *data)
 		data->leftright += 0.05;
 	if (keycode == 125)
 		data->leftright -= 0.05;
-	if (keycode == 13)
-		data->zoom -= 0.05;
+	if (keycode == 0)
+	{
+		data->zoom1 -= 0.1 * data->zoom1;
+		data->zoom2 -= 0.1 * data->mousex / (data->winwidth / 2)* data->zoom1;
+		data->zoom3 -= 0.1 * data->zoom3;
+		data->zoom4 -= 0.1 * data->mousey / (data->winwidth / 2)* data->zoom3;
+	}
 	if (keycode == 1)
-		data->zoom += 0.05;
-	printf("%d\n", keycode);
+	{
+		data->zoom1 += 0.1 * data->zoom1;
+		data->zoom2 += 0.1 * data->mousex / (data->winwidth / 2)* data->zoom1;
+		data->zoom3 += 0.1 * data->zoom3;
+		data->zoom4 += 0.1 * data->mousey / (data->winwidth / 2)* data->zoom3;
+	}
+	if (keycode == 49)
+		vardef(data);
+//	printf("%d\n", keycode);
 	algo(data);
 	return (0);
 }
 
-void	vardef(t_data *data)
+int	locmouse(int x, int y, t_data *data)
 {
-	data->winwidth = 600;
-	data->winlength = 600;
-	data->updown = 1;
-	data->leftright = 1;
-	data->zoom = 1;
+	if (x > 0 && x < data->winwidth && y > 0 && y < data->winlength)
+	{
+		data->mousex = x;
+		data->mousey = y;
+		printf("x: %d\n", x);
+		printf("y: %d\n", y);
+	}
+	
+	return (0);
 }
 
 int	main(void)
@@ -106,11 +136,13 @@ int	main(void)
 
 	data.mlx = mlx_init();
 	vardef(&data);
-	data.win = mlx_new_window(data.mlx, data.winwidth, data.winlength, "sussy baka");
+	data.win = mlx_new_window(data.mlx, data.winwidth, data.winlength, "fractol");
 	data.img = mlx_new_image(data.mlx, data.winwidth, data.winlength);
 	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel,
 			&data.line_length, &data.endian);
 	algo(&data);
+	mlx_hook(data.win, 06, 1L << 6, locmouse, &data);
 	mlx_key_hook(data.win, key_hook, &data);
+	//mlx_mouse_hook(vars.win, mouse_hook, &vars);
 	mlx_loop(data.mlx);
 }
